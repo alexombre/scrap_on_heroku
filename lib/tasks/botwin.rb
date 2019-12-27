6e135afe8d887c22cec42c52fa3efc4b94e24aec
Ubuntu <ubuntu@ip-172-31-15-180.ec2.internal>
2019-12-27 15:33:34 +0000

    add env firefox to scrapper

--- lib/tasks/botwin.rb
+++ lib/tasks/botwin.rb
@@ -8,6 +8,26 @@ require 'date'
 @proxy = {
     http: '159.8.114.37:8123'
 }
+require 'watir'
+require 'webdrivers'
+require 'pry'
+require 'selenium-webdriver'
+require 'json'
+require 'date'
+
+@proxy = {
+    http: '159.8.114.37:8123'
+}
+Selenium::WebDriver::Firefox::Binary.path=ENV['FIREFOX_BIN']
+b = Watir::Browser.new :firefox, headless: true, proxy: @proxy
+b.goto 'google.fr'
+
+
+
+
+
+binding.pry
+b.close
 b = Watir::Browser.new :firefox, headless: true, proxy: @proxy
 b.goto 'google.fr'
 
